c6b950efdebf3ae5f5acfa41eb50ce567c486704
Javier Lopez Lopez <javier@siclo.com>
2018-10-26 16:32:22 -0500

    Adding structu to create fake data

--- app/main.go
+++ app/main.go
@@ -1,24 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type Product struct {
+	ID     int    `bson:"_id"`
+	Title  string `json:"title"`
+	Image  string `json:"image"`
+	Price  uint64 `json:"price"`
+	Rating uint8  `json:"rating"`
+}
+
 func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "You have visited woooooo %s !", r.URL.Path)
+	fmt.Fprintf(w, "You have visited  %s !", r.URL.Path)
+}
+
+func handleGetClient(w http.ResponseWriter, r *http.Request) {
+
+	product := Product{1234, "This is the title", "This is the image", 98, 2}
+
+	p, err := json.Marshal(product)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(p)
 }
 
 func main() {
-	http.HandleFunc("/", handle)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handle).Methods("GET")
+	r.HandleFunc("/api/v1/getClient/", handleGetClient).Methods("GET")
+
 	fmt.Println("Starting Web app on 8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal(err)
 	}
 }
 
+/*
+api/v1/getClient
+*/
 // package main
 
 // import (
